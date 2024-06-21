99d25609d9be7ff111222ceac3308481e02b7e65
Sarfaroj Ansari <sarfarosha66@gmail.com>
2024-06-21 21:45:45 +0530

    Add rate limit and replace credentials with env variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -10,11 +11,21 @@ const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// …
+
 // mongoose setup
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  "mongodb+srv://sarfi07:sarfi2000@cluster0.vtnnxbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -7,9 +7,9 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dyzzyyud5",
-  api_key: "627344314757969",
-  api_secret: "UIdtBgnF1xgur5iPr9dnSpiyxvE",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // configure multer to store files
